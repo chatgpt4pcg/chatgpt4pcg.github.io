1724d534601b1c9da26007e2a0b937c899b2f061
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2023-03-26 14:29:13 +0900

    data: google search console

--- components/content/PageMeta.tsx
+++ components/content/PageMeta.tsx
@@ -19,6 +19,7 @@ export default function PageMeta({ pageTitle }: PageMetaProps) {
 				name='description'
 				content='The 1st ChatGPT4PCG Competition challenges participants to use ChatGPT to generate a Science Birds level resembling an English capital letter, with stability and similarity to the letter being evaluated through testing, and offers a chance for prompt engineers to demonstrate their creativity and skills.'
 			/>
+			<meta name="google-site-verification" content="zP3knJHTSO5RclPqlwXpxPbPT3Paw17OMhL69EbHW0E" />
 			<meta name='viewport' content='width=device-width, initial-scale=1' />
 		</Head>
 	);
