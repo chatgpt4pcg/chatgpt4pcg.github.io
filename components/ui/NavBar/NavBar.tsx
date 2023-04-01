c17174ce0c364da31df7a905a8f175b034a52c04
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2023-04-01 11:10:17 +0900

    feat: improve mobile nav ui and ux

--- components/ui/NavBar/NavBar.tsx
+++ components/ui/NavBar/NavBar.tsx
@@ -5,121 +5,128 @@ import {
 } from '@heroicons/react/24/outline';
 
 import Link from 'next/link';
+import React from 'react';
+import { RemoveScroll } from 'react-remove-scroll';
 import styles from './NavBar.module.css';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
 
 export default function NavBar() {
 	const router = useRouter();
-	const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+	const [isMobileNavOpen, setIsMobileNavOpen] = React.useState(false);
+
+	const ScrollStatus = isMobileNavOpen ? RemoveScroll : React.Fragment;
 
 	return (
-		<nav className={
-			isMobileNavOpen
-				? `${styles.navContainer} ${styles.mobileActive}`
-				: styles.navContainer
-		}>
-			<div className={styles.mobileNav}>
-				<Link href="/">ChatGPT4PCG</Link>
-				<button
-					className={styles.hamburger}
-					onClick={() => setIsMobileNavOpen((isOpen) => !isOpen)}
-				>
-					{isMobileNavOpen ? (
-						<XMarkIcon
-							style={{
-								width: '36px',
-								height: '36px',
-							}}
-						/>
-					) : (
-						<Bars3Icon
-							style={{
-								width: '36px',
-								height: '36px',
-							}}
-						/>
-					)}
-				</button>
-			</div>
-			<ul
+		<ScrollStatus>
+			<nav
 				className={
 					isMobileNavOpen
-						? `${styles.navigation} ${styles.mobileActive}`
-						: styles.navigation
+						? `${styles.navContainer} ${styles.mobileActive}`
+						: styles.navContainer
 				}
 			>
-				<li
-					className={`${styles.navItem} ${
-						router.pathname == '/' ? styles.active : ''
-					}`}
-				>
-					<Link href='/'>Home</Link>
-				</li>
-				<li
-					className={`${styles.navItem} ${
-						router.pathname == '/prizes' ? styles.active : ''
-					}`}
-				>
-					<Link href='/prizes'>Prizes</Link>
-				</li>
-				<li
-					className={`${styles.navItem} ${
-						router.pathname == '/prompt' ? styles.active : ''
-					}`}
-				>
-					<Link href='/prompt'>Prompt</Link>
-				</li>
-				<li
-					className={`${styles.navItem} ${
-						router.pathname == '/submission' ? styles.active : ''
-					}`}
-				>
-					<Link href='/submission'>Submission</Link>
-				</li>
-				<li
-					className={`${styles.navItem} ${
-						router.pathname == '/evaluation' ? styles.active : ''
-					}`}
-				>
-					<Link href='/evaluation'>Evaluation</Link>
-				</li>
-				<li
-					className={`${styles.navItem} ${
-						router.pathname == '/rules' ? styles.active : ''
-					}`}
-				>
-					<Link href='/rules'>Rules</Link>
-				</li>
-				<li
-					className={`${styles.navItem} ${
-						router.pathname == '/resources' ? styles.active : ''
-					}`}
-				>
-					<Link href='/resources'>Resources</Link>
-				</li>
-				<li
-					className={`${styles.navItem} ${
-						router.pathname == '/supplement' ? styles.active : ''
-					}`}
-				>
-					<Link href='/supplement'>Supplementary Material</Link>
-				</li>
-				<li className={`${styles.navItem} ${styles.special}`}>
-					<a
-						target='_blank'
-						href='https://github.com/chatgpt4pcg/chatgpt4pcg.github.io'
+				<div className={styles.mobileNav}>
+					<Link href='/'>ChatGPT4PCG</Link>
+					<button
+						className={styles.hamburger}
+						onClick={() => setIsMobileNavOpen((isOpen) => !isOpen)}
 					>
-						GitHub{' '}
-						<ArrowUpRightIcon
-							style={{
-								width: '12px',
-								height: '12px',
-							}}
-						/>
-					</a>
-				</li>
-			</ul>
-		</nav>
+						{isMobileNavOpen ? (
+							<XMarkIcon
+								style={{
+									width: '36px',
+									height: '36px',
+								}}
+							/>
+						) : (
+							<Bars3Icon
+								style={{
+									width: '36px',
+									height: '36px',
+								}}
+							/>
+						)}
+					</button>
+				</div>
+				<ul
+					className={
+						isMobileNavOpen
+							? `${styles.navigation} ${styles.mobileActive}`
+							: styles.navigation
+					}
+				>
+					<li
+						className={`${styles.navItem} ${
+							router.pathname == '/' ? styles.active : ''
+						}`}
+					>
+						<Link href='/'>Home</Link>
+					</li>
+					<li
+						className={`${styles.navItem} ${
+							router.pathname == '/prizes' ? styles.active : ''
+						}`}
+					>
+						<Link href='/prizes'>Prizes</Link>
+					</li>
+					<li
+						className={`${styles.navItem} ${
+							router.pathname == '/prompt' ? styles.active : ''
+						}`}
+					>
+						<Link href='/prompt'>Prompt</Link>
+					</li>
+					<li
+						className={`${styles.navItem} ${
+							router.pathname == '/submission' ? styles.active : ''
+						}`}
+					>
+						<Link href='/submission'>Submission</Link>
+					</li>
+					<li
+						className={`${styles.navItem} ${
+							router.pathname == '/evaluation' ? styles.active : ''
+						}`}
+					>
+						<Link href='/evaluation'>Evaluation</Link>
+					</li>
+					<li
+						className={`${styles.navItem} ${
+							router.pathname == '/rules' ? styles.active : ''
+						}`}
+					>
+						<Link href='/rules'>Rules</Link>
+					</li>
+					<li
+						className={`${styles.navItem} ${
+							router.pathname == '/resources' ? styles.active : ''
+						}`}
+					>
+						<Link href='/resources'>Resources</Link>
+					</li>
+					<li
+						className={`${styles.navItem} ${
+							router.pathname == '/supplement' ? styles.active : ''
+						}`}
+					>
+						<Link href='/supplement'>Supplementary Material</Link>
+					</li>
+					<li className={`${styles.navItem} ${styles.special}`}>
+						<a
+							target='_blank'
+							href='https://github.com/chatgpt4pcg/chatgpt4pcg.github.io'
+						>
+							GitHub{' '}
+							<ArrowUpRightIcon
+								style={{
+									width: '12px',
+									height: '12px',
+								}}
+							/>
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</ScrollStatus>
 	);
 }
