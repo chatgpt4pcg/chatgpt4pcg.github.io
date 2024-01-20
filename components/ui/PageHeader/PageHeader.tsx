0417e9fa2aa3b48e4f220180a8b109067d862bd4
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2024-01-20 14:51:55 +0900

    chore: update for GitHub Page

--- components/ui/PageHeader/PageHeader.tsx
+++
@@ -1,9 +0,0 @@
-import React, { PropsWithChildren } from 'react';
-
-import styles from './PageHeader.module.css';
-
-export default function PageHeader({ children }: PropsWithChildren) {
-	return <h1 className={styles.header}>
-    {children}
-  </h1>;
-}
--- components/ui/Section/Section.tsx
+++
@@ -1,7 +0,0 @@
-import React, { PropsWithChildren } from 'react';
-
-import styles from './Section.module.css';
-
-export default function Section({ children }: PropsWithChildren) {
-	return <section className={styles.section}>{children}</section>;
-}
