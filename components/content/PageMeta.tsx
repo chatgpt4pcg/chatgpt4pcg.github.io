e090c03f4d855c4b4fe2b84480a91ced97ff54db
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2024-02-08 13:11:51 +0900

    chore: update page meta

--- components/content/PageMeta.tsx
+++ components/content/PageMeta.tsx
@@ -17,9 +17,12 @@ export default function PageMeta({ pageTitle }: Readonly<PageMetaProps>) {
 			<title>{title}</title>
 			<meta
 				name='description'
-				content='The 2nd ChatGPT4PCG Competition challenges participants to use ChatGPT to generate a Science Birds level resembling an English capital letter, with stability and similarity to the letter being evaluated through testing, and offers a chance for prompt engineers to demonstrate their creativity and skills.'
+				content='The 2nd ChatGPT4PCG Competition challenges participants to use ChatGPT to generate diverse Science Birds levels resembling English capital letters, with stability and similarity to the letter being evaluated through testing, and offers a chance for prompt engineers to demonstrate their creativity and skills.'
 			/>
-			<meta name="google-site-verification" content="zP3knJHTSO5RclPqlwXpxPbPT3Paw17OMhL69EbHW0E" />
+			<meta
+				name='google-site-verification'
+				content='zP3knJHTSO5RclPqlwXpxPbPT3Paw17OMhL69EbHW0E'
+			/>
 			<meta name='viewport' content='width=device-width, initial-scale=1' />
 		</Head>
 	);
