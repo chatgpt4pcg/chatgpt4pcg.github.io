7f6ea4f2a6cd5398807caf18d94d73201f95254d
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2024-02-08 11:29:51 +0900

    content: update prompt page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,21 @@
 import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import { Bitter } from 'next/font/google';
 import { GoogleAnalytics } from 'nextjs-google-analytics';
+
+const bitter = Bitter({
+	subsets: ['latin'],
+	display: 'swap',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<GoogleAnalytics trackPageViews gaMeasurementId='G-RJKNZ831ZK' />
-			<Component {...pageProps} />
+			<main className={bitter.className}>
+				<Component {...pageProps} />
+			</main>
 		</>
 	);
 }
