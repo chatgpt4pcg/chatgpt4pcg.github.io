5cd08cb767427999d6590678f199a70f4fadf3db
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2024-02-10 10:43:47 +0900

    chore: update favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,27 @@ export default function Document() {
 	return (
 		<Html lang='en'>
 			<Head>
-				<link rel='shortcut icon' href='/favicon.ico' />
+				<link
+					rel='apple-touch-icon'
+					sizes='152x152'
+					href='/apple-touch-icon.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='32x32'
+					href='/favicon-32x32.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='16x16'
+					href='/favicon-16x16.png'
+				/>
+				<link rel='manifest' href='/site.webmanifest' />
+				<link rel='mask-icon' href='/safari-pinned-tab.svg' color='#13b2b9' />
+				<meta name='msapplication-TileColor' content='#13b2b9' />
+				<meta name='theme-color' content='#ffffff' />
 			</Head>
 			<body>
 				<Main />
